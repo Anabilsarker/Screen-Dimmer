1ed06a82506039c9ce241153901925928f0e8e4f
Anabilsarker <sarkeranabil@gmail.com>
2024-05-07 01:12:43 +0600

    Add project files.

---
+++ WpfApp1/DimmerOverlay.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace ScreenDimmer
+{
+    /// <summary>
+    /// Interaction logic for DimmerOverlay.xaml
+    /// </summary>
+    public partial class DimmerOverlay : Window
+    {
+        public DimmerOverlay()
+        {
+            InitializeComponent();
+            this.Height = SystemParameters.WorkArea.Height;
+            this.Width = SystemParameters.WorkArea.Width;
+        }
+    }
+}
